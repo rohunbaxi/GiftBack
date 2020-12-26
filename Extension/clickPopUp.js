5a9bb0d762a15a3126c7fb8b4bb2f2bea5023d82
Mahbod <mahbodsabbaghi@gmail.com>
2020-12-26 00:16:38 -0500

    -New Chrome Extension Look

---
+++ Extension/clickPopUp.js
@@ -0,0 +1,38 @@
+function expand(obj){
+    obj.classList.add('expand')
+}
+
+function contract() {
+    var divList = document.getElementsByTagName("div")
+    for (var i = 0; i < divList.length; i++){
+        if (divList[i].className === "email expand"){
+            divList[i].className = "email"
+        }
+    }
+    event.stopPropagation();
+}
+
+function modifyPage(elements) {
+    for (var i = 0; i <elements.length; i++){
+        var tempPrice = elements[i].innerHTML
+        console.log(tempPrice)
+        var tempPopup = document.getElementsByClassName("popUp")[0]
+        elements[i].innerHTML = (nodeToString(createPopUp(tempPrice, tempPopup)))
+    }
+}
+
+function createPopUp(price, popUpInstance) {
+    popUpInstance.getElementsByClassName("name")[0].innerText = price
+    popUpInstance.getElementsByClassName("name-large")[0].innerText = price
+    return popUpInstance
+}
+
+function nodeToString (node) {
+    var tmpNode = document.createElement("div");
+    tmpNode.appendChild(node.cloneNode(true));
+    return tmpNode.innerHTML;
+}
+
+var amazonTags = document.getElementsByClassName("amazon")
+console.log(amazonTags)
+modifyPage(amazonTags)
